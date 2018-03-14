1f86c0a525df196800089a59c4bffb408c3f2d28
taranesi20 <37382926+taranesi20@users.noreply.github.com>
2018-03-14 23:16:59 +0330

    Update 5_1_logic_operator_on_booleans.py

--- 5_1_logic_operator_on_booleans.py
+++ 5_1_logic_operator_on_booleans.py
@@ -1,11 +1,11 @@
-a= True
+a= False
 b= False
-print("a is {0}".format(a))
-print("b is {0}".format(b))
-print("~a is {0}".format(not a))
-print("~b is {0}".format(not b))
-print("a & b is {0}".format(a and b)) # True if both of them are True
-print("a or b is {0}".format(a or b)) # True if one or both of them are True
+print("a is {0}".format(a and b))
+print("b is {0}".format(b and a))
+print("~a is {0}".format(not a or a))
+print("~b is {0}".format(not b or a))
+print("a & b is {0}".format(a and b and a)) # True if both of them are True
+print("a or b is {0}".format(a or b or a)) # True if one or both of them are True
 
 
 #################################################
