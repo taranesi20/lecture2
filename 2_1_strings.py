5560e88d212710148fba646700fd7f186f184d6c
taranesi20 <37382926+taranesi20@users.noreply.github.com>
2018-03-14 23:13:48 +0330

    Update 2_1_strings.py

--- 2_1_strings.py
+++ 2_1_strings.py
@@ -1,33 +1,34 @@
 ####################################
 # following variables are the same #
 ####################################
-# str1=" salam "
-# str2='salam'
+ str1=" salam! "
+ str2='salam!'
 
 ######################
 ## concat two string #
 ######################
-# name='mohammad'
-# greeting= str1+name
-# print(greeting)
-#
-# greeting2=str1+" "+name
-# print(greeting2)
-#
-# print(("\'"+greeting2+" \'") * 3)
+ name='tarane'
+ family='andalib'
+ greeting= str1+name+family
+ print(greeting)
+
+ greeting2=str1+" "+name+" "+famliy
+ print(greeting2)
+
+ print(("\'"+greeting2+" \'") * 5)
 ######################################
 ## some useful operations on strings #
 ######################################
-# print(greeting2.capitalize())
-# print(greeting2.upper())
-# print(greeting2.rsplit(' ')) # splitting the string by blank
-# print(greeting2.strip(' ')) # remove the blanks before and after the string
+ print(greeting2.capitalize())
+ print(greeting2.upper())
+ print(greeting2.rsplit(' ')) # splitting the string by blank
+ print(greeting2.strip(' ')) # remove the blanks before and after the string
 
 ###########################################
 ### another sample of printing a varibale #
 ###########################################
-pi = 3.14
-print(pi)
-str_pi = str(pi)
-print("pi number is : ", pi)  # print each part of input separately
-print("pi number is : " + str(pi))  # concat two string
+a = 256.652
+print(a)
+str_a = str(a)
+print("a number is : ", a)  # print each part of input separately
+print("a number is : " + str(a))  # concat two string
